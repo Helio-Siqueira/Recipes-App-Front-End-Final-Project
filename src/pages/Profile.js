c3c286d7af5c95eae02d8588bfba9fb49a9c1797
Thariklz <thariklz19@gmail.com>
2022-06-22 16:58:37 -0300

    requisitos 82, 83, 84- correto

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { getEmail } from '../services/LocalStorage';
 
 function Profile() {
+  const email = getEmail();
+  console.log(email);
   return (
-    <div>
+    <div className="profile__container">
       <Header />
+      <h3 data-testid="profile-email">{ email.email }</h3>
+
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+      >
+        Done Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
