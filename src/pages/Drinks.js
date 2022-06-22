0cb491f166c2479e01aeb65ee6ca1fa64c10fdca
Lucca Rendall <luccarendall2@gmail.com>
2022-06-22 06:30:17 -0300

    Footer pronto

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Drinks() {
   return (
     <div>
       <Header />
       <div>Drinks</div>
+      <Footer />
     </div>
   );
 }
--- src/pages/Nationalities.js
+++ src/pages/Nationalities.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Nationalities() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
