305ababb4222897b66e2164100470087ee8c86e3
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-06-22 09:21:24 -0300

    Requisito 27

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Drinks() {
+  const [categoryDrink, setCategoryDrink] = useState([]);
+  const [magigNumber] = useState('5');
+  useEffect(() => {
+    async function getCategorysDrink() {
+      try {
+        const endopint = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+        const response = await fetch(endopint);
+        const { drinks: drink } = await response.json();
+        setCategoryDrink(drink);
+      } catch (error) {
+        return error;
+      }
+    }
+
+    getCategorysDrink();
+  }, []);
   return (
     <div>
       <Header />
       <div>Drinks</div>
+      <section>
+        { categoryDrink.map((category, index) => (
+          <button
+            data-testid={ `${category.strCategory}-category-filter` }
+            type="button"
+            key={ index }
+          >
+            { category.strCategory }
+
+          </button>
+        )).slice(0, Number(magigNumber)) }
+      </section>
       <Footer />
     </div>
   );
 }
 
 export default Drinks;
-// ronan
