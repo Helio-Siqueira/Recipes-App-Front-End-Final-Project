52199539acbdf9a52d789320e9a45eff5ad9ccfa
Lucca Rendall <luccarendall2@gmail.com>
2022-06-28 16:46:12 -0300

    Requisitos 75 e 76

--- src/pages/IngredientsDrinks.js
+++ src/pages/IngredientsDrinks.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function IngredientsDrinks() {
-  return (
+  const [ingredients, setIngredients] = useState([]);
+  console.log(ingredients);
 
+  useEffect(() => {
+    async function getIngredients() {
+      try {
+        const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
+        const response = await fetch(url);
+        const { drinks } = await response.json();
+        console.log(drinks);
+        const TWELVE = 12;
+        const newListIngredients = drinks.slice(0, TWELVE);
+        console.log(newListIngredients);
+        setIngredients(newListIngredients);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getIngredients();
+  }, []);
+
+  return (
     <div>
       <Header />
+
+      {ingredients.map((item, index) => (
+        <div key={ index } data-testid={ `${index}-ingredient-card` }>
+          <p data-testid={ `${index}-card-name` }>
+            {`${item.strIngredient1}`}
+          </p>
+          <img
+            data-testid={ `${index}-card-img` }
+            src={ `https://www.thecocktaildb.com/images/ingredients/${item.strIngredient1}-Small.png` }
+            alt="imagem dos ingredientes"
+          />
+        </div>
+      ))}
+
       <Footer />
     </div>
   );
--- src/pages/IngredientsFoods.js
+++ src/pages/IngredientsFoods.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function IngredientsFoods() {
+  const [ingredients, setIngredients] = useState([]);
+  console.log(ingredients);
+
+  useEffect(() => {
+    async function getIngredients() {
+      try {
+        const url = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+        const response = await fetch(url);
+        const { meals } = await response.json();
+        // console.log('retorno da API:');
+        // console.log(meals);
+        const TWELVE = 12;
+        const newListIngredients = meals.slice(0, TWELVE);
+        console.log(newListIngredients);
+        setIngredients(newListIngredients);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getIngredients();
+  }, []);
+
   return (
     <div>
       <Header />
+
+      {ingredients.map((item, index) => (
+        <div key={ index } data-testid={ `${index}-ingredient-card` }>
+          <p data-testid={ `${index}-card-name` }>
+            {`${item.strIngredient}`}
+          </p>
+          <img
+            data-testid={ `${index}-card-img` }
+            src={ `https://www.themealdb.com/images/ingredients/${item.strIngredient}-Small.png` }
+            alt="imagem dos ingredientes"
+          />
+        </div>
+      ))}
+
       <Footer />
     </div>
   );
 }
+
+// IngredientsFoods.propTypes = {
+//   food: PropTypes.objectOf(
+//     PropTypes.shape({
+//       strMeal: PropTypes.string.isRequired,
+//       strMealThumb: PropTypes.string.isRequired,
+//     }),
+//   ).isRequired,
+//   idTest: PropTypes.string.isRequired,
+// };
 
 export default IngredientsFoods;
