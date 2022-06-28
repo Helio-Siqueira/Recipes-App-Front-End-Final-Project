b232e308d1bec068e7be1547fa813ff7c88681a0
Lucca Rendall <luccarendall2@gmail.com>
2022-06-28 16:48:46 -0300

    mudando para a branch certa

--- src/pages/IngredientsFoods.js
+++ src/pages/IngredientsFoods.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Link, useHistory } from 'react-router-dom';
 
 function IngredientsFoods() {
   const [ingredients, setIngredients] = useState([]);
