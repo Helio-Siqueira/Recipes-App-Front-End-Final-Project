4a298cdc4d25ac1474b573c360adda5d74fa82a0
Thariklz <thariklz19@gmail.com>
2022-07-01 19:57:22 -0300

    pagina de login

--- src/pages/Logo.js
+++ src/pages/Logo.js
@@ -1,23 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Logo from '../images/logomarca-01.svg';
 
 function LogoMarca() {
-  const [visible, setVisible] = useState(true);
-
-  useEffect(() => {
-    const number = 2000;
-    const timer = setTimeout(setVisible(false), number);
-    timer();
-  }, []);
-
-  useEffect(() => {
-    clearTimeout(timer);
-  }, [visible]);
   return (
     <div>
-      {
-        visible && <img src={ Logo } alt="Taste e Travel" />
-      }
+      <img src={ Logo } alt="Taste e Travel" className="logo__animation" />
     </div>
   );
 }
